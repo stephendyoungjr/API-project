34d5736efd82a3f4bd9be07122b0c162f137d3c5
stephendyoungjr <stephenyoung.np@outlook.com>
2024-06-20 20:07:22 -0400

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
-
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
@@ -18,3 +20,7 @@ router.get("/api/csrf/restore", (req, res) => {
 });
 
 module.exports = router;
+
+
+
+
